abebcc5c0335e51b4422b703638b025e3311ef68
Steven Velez <stevendvelez9@gmail.com>
2020-09-22 21:12:43 -0400

    add carousel to jumbotron

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Carousel from "../UI/Carousel/Carousel";
 import CTAButton from "../UI/CTAButton/CTAButton";
 
 import classes from "./Jumbotron.module.css";
@@ -8,38 +7,51 @@ import classes from "./Jumbotron.module.css";
 const Jumbotron = () => {
     return (
         <div className={classes.Jumbotron}>
-            {/* <div className={classes.Carousel}>
-                <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+            <div className={classes.Carousel}>
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <div className={classes.Text}>
-                                <h1></h1>
-                                <p></p>
+                            <div className={classes.Image1}>
+                                <div className={classes.Text1}>
+                                    <h1>DON'T LIVE ON THE <em>edge!</em></h1>
+                                    <p>Allow one of our fully licensed inspectors to asses your home</p>
+                                    <CTAButton>Schedule Your Inspection</CTAButton>
+                                </div>
                             </div>
-                            <img src="" className="d-block" alt="testimonial" />
                         </div>
                         <div className="carousel-item">
-                            <div className={classes.Text}>
-                            <h1></h1>
-                            <p></p>
+                            <div className={classes.Image2}>
+                                <div className={classes.Text2}>
+                                    <h1>Standard Home Inspetions</h1>
+                                    <p>Just bought a home? Let us make sure it's up to your standards</p>
+                                    <CTAButton>Schedule Your Inspection</CTAButton>
+                                </div>
                             </div>
-                            <img src="" className="d-block" alt="testimonial" />
                         </div>
                         <div className="carousel-item">
-                            <div className={classes.Text}>
-                            <h1></h1>
-                            <p></p>
+                            <div className={classes.Image3}>
+                                <div className={classes.Text3}>
+                                    <h1>Superior Service</h1>
+                                    <p>Quality inspections. Quality customer service</p>
+                                    <CTAButton>Schedule Your Inspection</CTAButton>
+                                </div>
                             </div>
-                            <img src="" className="d-block" alt="testimonial" />
                         </div>
                     </div>
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
-            </div> */}
-            
-            <div className={classes.Text}>
-                <h1>DON'T LIVE ON THE <em>edge!</em></h1>
-                <p>Allow one of our fully licensed inspectors to asses your home</p>
-                <CTAButton>Schedule Your Inspection</CTAButton>
             </div>
         </div>
     );
