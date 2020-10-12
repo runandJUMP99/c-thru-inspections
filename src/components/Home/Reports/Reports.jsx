145d855a01c7a24e91873e5179875bbac57d3f39
Steven Velez <stevendvelez9@gmail.com>
2020-10-12 11:13:25 -0400

    update all company names to include hyphen

--- src/components/Home/Reports/Reports.jsx
+++ src/components/Home/Reports/Reports.jsx
@@ -12,7 +12,7 @@ const Reports = () => {
                 <div className={classes.ContentContainer}>
                     <div className={classes.Text}>
                         <h2>Detailed Home Inspection Reports</h2>
-                        <p><strong>C Thru Inspections</strong> provides detailed reports for all of our inspections. We utilize the most up to date applications that allow us to navigate through the house and break down some of the most important components of your home. This provides us the ability to deliver you a detailed inspection report within 24-48 hours, and sometimes even within the same day!  </p>
+                        <p><strong>C-Thru Inspections</strong> provides detailed reports for all of our inspections. We utilize the most up to date applications that allow us to navigate through the house and break down some of the most important components of your home. This provides us the ability to deliver you a detailed inspection report within 24-48 hours, and sometimes even within the same day!  </p>
                         {/* <CTAButton>See Example Report</CTAButton> */}
                     </div>
                     <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/modern-technologies-1263422_1920.jpg?alt=media&token=ed5f0a20-cc67-43d5-b46d-c6cce890b06c" alt="Reports"/>
