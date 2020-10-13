cdeea3d15dfc50a236cbe99244305846f2efb2a2
Steven Velez <stevendvelez9@gmail.com>
2020-10-13 18:00:50 -0400

    add analytics event to CTAButton componenet

--- src/components/UI/CTAButton/CTAButton.jsx
+++ src/components/UI/CTAButton/CTAButton.jsx
@@ -5,7 +5,7 @@ import classes from "./CTAButton.module.css";
 const CTAButton = (props) => {
     return (
         <button className={classes.CTAButton} disabled={props.disabled}>
-            <a href={props.link}>{props.children}</a>
+            <a className={classes.Link} href={props.link}>{props.children}</a>
         </button> 
     );
 }
