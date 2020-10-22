0e95eb4f3fa500840f95c66cbffb190505126a92
Steven Velez <stevendvelez9@gmail.com>
2020-10-22 09:18:08 -0400

    add fade effect to services logos on home page; update horizontal logo

--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Fade from "react-reveal/Fade";
 import Service from "./Service/Service";
 
 import classes from "./Services.module.css";
@@ -13,11 +14,13 @@ const Services = () => {
                 <Service icon={"fas fa-wind"} service="Wind Mitigation Inspection" description="Hurricanes are common in Florida. This inspection assists in determining how vulnerable your home is to high velocity winds." />
                 <Service icon={"fas fa-bullseye"} service="4-Point Inspection" description="Rest easy knowing that the four major components of your home: roofing, plumbing, electrical, and HVAC systems, are secure." />
             </div>
-            <div className={classes.Logos}>
-                <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/CPI-Logo.png?alt=media&token=1c761a2d-1c00-49db-9ec6-94f915efdaec" alt="CPI Logo"/>
-                <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Wind%20Mitigation-Logo.png?alt=media&token=11296278-04da-4455-aef3-fb79b780f079" alt="Wind Mitigation Logo"/>
-                <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Four%20Point-Logo.png?alt=media&token=cf4719db-2c65-471c-b284-08def6a13364" alt="Four Point Logo"/>
-            </div>
+            <Fade bottom>
+                <div className={classes.Logos}>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/CPI-Logo.png?alt=media&token=1c761a2d-1c00-49db-9ec6-94f915efdaec" alt="CPI Logo"/>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Wind%20Mitigation-Logo.png?alt=media&token=11296278-04da-4455-aef3-fb79b780f079" alt="Wind Mitigation Logo"/>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Four%20Point-Logo.png?alt=media&token=cf4719db-2c65-471c-b284-08def6a13364" alt="Four Point Logo"/>
+                </div>
+            </Fade>
         </div>
     );
 }
--- src/components/UI/Logo/LongLogo/LongLogo.jsx
+++ src/components/UI/Logo/LongLogo/LongLogo.jsx
@@ -5,7 +5,7 @@ import classes from "./LongLogo.module.css";
 const LongLogo = () => {
     return (
         <a className={classes.LongLogo} href="/">
-            <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Logo-Horizontal.svg?alt=media&token=89e2739e-b484-470f-b142-f792e30b1aca" alt="C Thru Inspections LLC"/>
+            <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/Logo-Horizontal.svg?alt=media&token=57c84ca6-cbeb-4705-bf0f-0fefee0743fa" alt="C Thru Inspections LLC"/>
         </a>
     );
 }
