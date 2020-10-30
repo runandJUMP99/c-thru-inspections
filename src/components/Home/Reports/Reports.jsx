8e98ccc8cc6dc21df59827b102eafcdcd7c4bd0c
Steven Velez <stevendvelez9@gmail.com>
2020-10-30 13:40:20 -0400

    updated jumbotron text for image1; updated reports image; adjust text width on jumbotron for larger devices

--- src/components/Home/Reports/Reports.jsx
+++ src/components/Home/Reports/Reports.jsx
@@ -15,7 +15,7 @@ const Reports = () => {
                         <p><strong>C Thru Inspections</strong> provides detailed reports for all of our inspections. We utilize the most up to date applications that allow us to navigate through the house and break down some of the most important components of your home. This provides us the ability to deliver you a detailed inspection report within 24-48 hours, and sometimes even within the same day!  </p>
                         {/* <CTAButton>See Example Report</CTAButton> */}
                     </div>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/modern-technologies-1263422_1920.jpg?alt=media&token=ed5f0a20-cc67-43d5-b46d-c6cce890b06c" alt="Reports"/>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/markus-spiske-SRNKol31s64-unsplash.jpg?alt=media&token=10c9dcc9-6067-47ca-9cc9-28bff305a455" alt="Reports"/>
                 </div>
             </Fade>
         </div>
