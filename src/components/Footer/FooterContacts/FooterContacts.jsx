aa244a826da0ae963f9f004a474bf51c1a30769c
Steven Velez <stevendvelez9@gmail.com>
2020-11-06 12:20:50 -0500

    add yelp link to footer; add What To Expect section in services page

--- src/components/Footer/FooterContacts/FooterContacts.jsx
+++ src/components/Footer/FooterContacts/FooterContacts.jsx
@@ -16,6 +16,7 @@ const FooterContacts = () => {
                     <p><i className="fas fa-map-marker-alt"></i>Proudly Serving the Central Florida Area</p>
                     <p><i className="fas fa-clock"></i>Monday - Saturday: 8:00am - 5:00pm</p>
                     <div className={classes.SocialLinks}>
+                        <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://yelp.com/biz/c-thru-inspections-kissimmee"><i class="fab fa-yelp"></i></a></p>
                         <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/C-Thru-Inspections-LLC-103441381576988"><i class="fab fa-facebook"></i></a></p>
                         <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/holiver-estrada-62b1141bb/"><i class="fab fa-linkedin"></i></a></p>
                     </div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 import Bundle from "./Bundle/Bundle";
 import CTASection from "../CTASection/CTASection";
@@ -8,6 +9,11 @@ import Service from "./Service/Service";
 import classes from "./Services.module.css";
 
 const Services = () => {
+    function scrollTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
     return (
         <div className={classes.Services}>
             <Intro />
@@ -15,6 +21,12 @@ const Services = () => {
                 <Service title="Home Inspection" icon="fas fa-house-damage" startingAt="Starting at: " price="295" description="A Home Inspection is a non-invasive examination of the condition of the home. Our Home Inspection consists of inspecting the most important components of a home, such as the foundation, frame, and roof, among others. Home Inspections typically take between 2 to 3 1/2 hours." />
                 <Service title="Wind Mitigation Inspection" icon="fas fa-wind" price="85" description="A Wind Mitigation inspection, also called a Windstorm Mitigation Inspection, is when a Certified Inspector checks your home's wind resistant features. Though a wind mitigation inspection is not required to get for Florida Homeowners Insurance, it can result in substantial savings for your household." />
                 <Service title="4-Point Inspection" icon="fas fa-bullseye" price="100" description="A 4-Point Inspection looks at the four major systems of your home: roof, electrical, plumbing, and HVAC. An insurance company wants to know that your home has been well maintained and the major systems are in solid, working condition." />
+                <h1 className={classes.ExplanationHeader}>What To Expect From Your Home Inspection</h1>
+                <p className={classes.Explanation}>
+                    With <strong>C Thru Inspections</strong>, expect that your home will be serviced by a fully certified, professional inspector using the most up-to-date equipment and software to evaluate the most important components of your home. Our inspectors will look over vital elements, such as the structural foundation, safety hazards, and parts of your home that may require maintenance to avoid future damage.
+                    <br/><br/>
+                    <strong>C Thru Inspections</strong> also provides full, easy to understand reports that will be available to you within 24 hours of your inspection. Still have questions about your inspection? Feel free to <NavLink onClick={scrollTop} to="/contactus">contact us!</NavLink> We will be more than happy to assist in any way we can. Our inspectors will also practice safe, social distancing and wear Personal Protective Equipment (PPE) for your comfort. We look forward to working with you!
+                </p>
             </div>
             <Bundle />
             <CTASection />
