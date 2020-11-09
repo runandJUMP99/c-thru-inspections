b4a02c79196427ad1646638dad9f7d3d4a2171ef
Steven Velez <stevendvelez9@gmail.com>
2020-11-09 16:44:34 -0500

    add instagram social to footer

--- src/components/Footer/FooterContacts/FooterContacts.jsx
+++ src/components/Footer/FooterContacts/FooterContacts.jsx
@@ -18,6 +18,7 @@ const FooterContacts = () => {
                     <div className={classes.SocialLinks}>
                         <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://yelp.com/biz/c-thru-inspections-kissimmee"><i class="fab fa-yelp"></i></a></p>
                         <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/C-Thru-Inspections-LLC-103441381576988"><i class="fab fa-facebook"></i></a></p>
+                        <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/c_thru_inspections/"><i class="fab fa-instagram"></i></a></p>
                         <p><a className={classes.SocialLink} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/holiver-estrada-62b1141bb/"><i class="fab fa-linkedin"></i></a></p>
                     </div>
                 </div>
