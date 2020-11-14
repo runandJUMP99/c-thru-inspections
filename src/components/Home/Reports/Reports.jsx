9c6c794e048f019061e75df1f6cce2529ac73e0f
Steven Velez <stevendvelez9@gmail.com>
2020-11-14 18:04:47 -0500

    updated text to reports section on home page

--- src/components/Home/Reports/Reports.jsx
+++ src/components/Home/Reports/Reports.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-// import CTAButton from "../../UI/CTAButton/CTAButton";
 import Fade from "react-reveal/Fade";
 
 import classes from "./Reports.module.css";
@@ -12,7 +11,7 @@ const Reports = () => {
                 <div className={classes.ContentContainer}>
                     <div className={classes.Text}>
                         <h2>Detailed Home Inspection Reports</h2>
-                        <p><strong>C Thru Inspections</strong> provides detailed reports for all of our inspections. We utilize the most up to date applications that allow us to navigate through the house and break down some of the most important components of your home. This provides us the ability to deliver you a detailed inspection report within 24-48 hours, and sometimes even within the same day!  </p>
+                        <p><strong>C Thru Inspections</strong> provides detailed reports for all of our inspections. We utilize the most up to date applications that allow us to navigate through the house and break down some of the most important components of your home. This provides us the ability to deliver you a detailed inspection report that you can easily view from your computer, tablet, or smartphone within 24-48 hours, and sometimes even within the same day!  </p>
                         <a target="_blank" rel="noopener noreferrer" href="https://www.spectora.com/home-inspectors/c-thru-inspections-llc/sample_report?sample_id=5613">View Sample Report</a>
                     </div>
                     <img src="https://firebasestorage.googleapis.com/v0/b/c-thru-inspections.appspot.com/o/markus-spiske-SRNKol31s64-unsplash.jpg?alt=media&token=10c9dcc9-6067-47ca-9cc9-28bff305a455" alt="Reports"/>
