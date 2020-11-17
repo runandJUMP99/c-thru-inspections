fa8a83143f7f584c0ecfaf7e2593915019c13d8b
Steven Velez <stevendvelez9@gmail.com>
2020-11-17 12:44:47 -0500

    update price on services page

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -19,7 +19,7 @@ const Services = () => {
         <div className={classes.Services}>
             <Intro />
             <div className={classes.ServiceGroup}>
-                <Service title="Home Inspection" icon="fas fa-house-damage" startingAt="Starting at: " price="295" description="A Home Inspection is a non-invasive examination of the condition of the home. Our Home Inspection consists of inspecting the most important components of a home, such as the foundation, frame, and roof, among others. Home Inspections typically take between 2 to 3 1/2 hours." />
+                <Service title="Home Inspection" icon="fas fa-house-damage" startingAt="Starting at: " price="275" description="A Home Inspection is a non-invasive examination of the condition of the home. Our Home Inspection consists of inspecting the most important components of a home, such as the foundation, frame, and roof, among others. Home Inspections typically take between 2 to 3 1/2 hours." />
                 <Service title="Wind Mitigation Inspection" icon="fas fa-wind" price="85" description="A Wind Mitigation inspection, also called a Windstorm Mitigation Inspection, is when a Certified Inspector checks your home's wind resistant features. Though a wind mitigation inspection is not required to get for Florida Homeowners Insurance, it can result in substantial savings for your household." />
                 <Service title="4-Point Inspection" icon="fas fa-bullseye" price="100" description="A 4-Point Inspection looks at the four major systems of your home: roof, electrical, plumbing, and HVAC. An insurance company wants to know that your home has been well maintained and the major systems are in solid, working condition." />
                 <Fade>
